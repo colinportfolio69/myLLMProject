8e3a136377d40d37b961361994e748deb2cfff65
cleal <7068col@gmail.com>
2023-12-11 17:13:16 +0100

    Re-write tokenizer notebook based on new understanding.

---
+++ tokenize.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+'''
+Aim:
+Read Wizard of Oz. Tokenize its text data on a character-level. 
+Take user input to both encode into tokens then decode back to original message.
+
+Navigate to tokenizer.ipynb for a detailed explanation of the script's workings.
+'''
